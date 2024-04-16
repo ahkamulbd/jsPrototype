d3d6489cdce8e9c5711deb9b198e2273383e835c
ahkamul <sasraishougat@gmail.com>
2024-04-16 13:55:11 +0600

    New object accessed named sakib & tamim from Parent function Person

--- js/prototype.js
+++ js/prototype.js
@@ -1,13 +1,35 @@
-let person= {};
+// let person= {};
 
-person.name = 'Sappo';
-person.age = 7;
+// person.name = 'Sappo';
+// person.age = 7;
 
-person.eat = function(){
-    console.log(`Person is eating`);
+// person.eat = function(){
+//     console.log(`Person is eating`);
+// }
+// person.sleep = function(){
+//     console.log(`Person is sleeping`);
+// }
+
+// console.log(person);
+
+function Person(name, age){
+    let person= {};
+
+    person.name = name;
+    person.age = age;
+
+    person.eat = function(){
+        console.log(`Person is eating`);
+    }
+    person.sleep = function(){
+        console.log(`Person is sleeping`);
+    }
+
+    return person;
 }
-person.sleep = function(){
-    console.log(`Person is sleeping`);
-}
 
-console.log(person);
+let sakib = new Person('Sakib', 35);
+let tamim = new Person('Tamim', 36);
+
+console.log(sakib);
+console.log(tamim)
