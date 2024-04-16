5004b512a080dacb38bc0f50f071314250e01365
ahkamul <sasraishougat@gmail.com>
2024-04-16 11:12:32 +0600

    js folder and index.html created & initial person object written in prototype.js

---
+++ js/prototype.js
@@ -0,0 +1,13 @@
+let person= {};
+
+person.name = 'Sappo';
+person.age = 7;
+
+person.eat = function(){
+    console.log(`Person is eating`);
+}
+person.sleep = function(){
+    console.log(`Person is sleeping`);
+}
+
+console.log(person);
